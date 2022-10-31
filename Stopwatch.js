91a07faebd9efefac4a9c111c962339841b2716c
shahanavazm <63035638+shahanavazm@users.noreply.github.com>
2022-10-31 14:28:47 -0400

    Update Stopwatch.js
    rewrote using the style in thinking in react example

--- Stopwatch.js
+++ Stopwatch.js
@@ -1,44 +1,44 @@
 import { useState } from "react";
 import { useInterval } from "./util";
 
-function StopwatchLcl({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [delay, setDelay] = useState(null);
-
-  useInterval(() => {
-    setCount(c => c + 1);
-  }, delay);
-
-  function handleStartClick() {
-    setDelay(1);
-  }
-
-  function handleStopClick() {
-    setDelay(null);
-  }
-
-  function handleResetClick() {
-    setCount(initialCount);
-  }
-
-  // render inputs:
-  // - count
-  // - handleStartClick
-  // - handleStopClick
-  // - handleResetClick
+function StopwatchPure({ count, onStartClick, onStopClick, onResetClick }) {
   return (
     <>
       {count}
       <br />
-      <button onClick={handleStartClick}>Start</button>
-      <button onClick={handleStopClick}>Stop</button>
-      <button onClick={handleResetClick}>Reset</button>
+      <button onClick={onStartClick}>Start</button>
+      <button onClick={onStopClick}>Stop</button>
+      <button onClick={onResetClick}>Reset</button>
     </>
   );
 }
 
-function Stopwatch() {
-  return <StopwatchLcl initialCount={0} />;
-}
+export default function Stopwatch() {
+  const [count, setCount] = useState(0);
+  const [delay, setDelay] = useState(null);
 
-export default Stopwatch;
+  useInterval(() => {
+    setCount((c) => c + 1);
+  }, delay);
+
+  function start() {
+    setDelay(1);
+  }
+
+  function stop() {
+    setDelay(null);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+    <StopwatchPure
+      count={count}
+      onStartClick={start}
+      onStopClick={stop}
+      onResetClick={reset}
+    />
+  );
+}
