65a73d7fdcb9bd85d342a0e02f8c1012f415ff0f
shahanavazm <63035638+shahanavazm@users.noreply.github.com>
2022-11-03 00:03:01 -0400

    Update Todo.js
    rewrote using the example thinking in react as guide.

--- Todo.js
+++ Todo.js
@@ -1,79 +1,8 @@
 // Display a checklist with multiple options that can select
 // and the selected options are dynamically displayed on the screen.
 
-// lessons learnt:
-// 1. Never change props in receiving components.
-// Especially arrays and arrays of objects.
-// They may be changed in the parent that owns the prop.
-// Even in parent change them immutably.
-// Also, just changing props in parent, does not cause re-render.
-// For re=render to happen a state needs to be defined, and setState called.
-// 2. When state is updated never mutate previous state, always change it immutably.
-// 3. When arrays are displayed using map, try not to delete or rearrange items.
-// Its okay to append items. This is because, the index is used as key by default
-// and it works well without deletes and rearranges. If deletes and rearranges are needed
-// then try to come up with a a unique key for each item in array and set as key.
-// In most cases it is just simple to avoid deletes and rearranges.
-// 4. Regarding previous point, It is much better to generate unique keys using nanoid
-// during creation of the array items, instead of using index. Don't generate nanoid
-// during rendering as that will cause components to be re=created for each re-render.
-
 import { useState } from "react";
 import { nanoid } from "nanoid";
-
-function useTodoBar(onSubmit) {
-  const [barText, setBarText] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    onSubmit(barText);
-    setBarText("");
-  }
-
-  // render inputs:
-  // - barText
-  // - setBarText
-  // - handleSubmit
-  const barContent = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={barText}
-        onChange={(e) => setBarText(e.target.value)}
-      />
-    </form>
-  );
-
-  return barContent;
-}
-
-function DisplayTodos({ todos }) {
-  function displayTodo(item, index) {
-    return <li key={item.id}>{item.str}</li>;
-  }
-
-  const style = { display: "table-cell", listStyleType: "none" };
-  return <ul style={style}>{todos.map(displayTodo)}</ul>;
-}
-
-function DisplayCheckBoxes({ todos, setTodosRemove }) {
-  function displayCheckBox(item, index) {
-    function setTodosRemoveI() {
-      setTodosRemove(index);
-    }
-
-    const handleClick = setTodosRemoveI;
-
-    return (
-      <li key={item.id}>
-        <input type="checkbox" onClick={handleClick} />
-      </li>
-    );
-  }
-
-  const style = { display: "table-cell", listStyleType: "none" };
-  return <ul style={style}>{todos.map(displayCheckBox)}</ul>;
-}
 
 function fetchTodos() {
   const todos = [
@@ -84,51 +13,77 @@ function fetchTodos() {
   return todos;
 }
 
-function TodoLcl() {
-  const [todos, setTodos] = useState(fetchTodos());
-
-  function setTodosAdd(todoStr) {
-    if (!todoStr) {
-      return;
-    }
-    // make a new array, otherwise no re-render.
-    const newTodos = [...todos, { str: todoStr, id: nanoid() }];
-    setTodos(newTodos);
+function TodoPure({
+  todoText,
+  todos,
+  onTodoTextChange,
+  onTodoTextSubmit,
+  onResetClick,
+  onIsDoneChange,
+}) {
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    onTodoTextSubmit();
   }
 
-  function setTodosRemove(index) {
-    // make a new array, otherwise no re-render.
+  return (
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          value={todoText}
+          onChange={(e) => onTodoTextChange(e.target.value)}
+        />
+      </form>
+      <ul style={{ display: "table-cell", listStyleType: "none" }}>
+        {todos.map((item, index) => (
+          <li key={item.id}>
+            <input type="checkbox" onClick={() => onIsDoneChange(index)} />
+          </li>
+        ))}
+      </ul>
+      <ul style={{ display: "table-cell", listStyleType: "none" }}>
+        {todos.map((item) => (
+          <li key={item.id}>{item.str}</li>
+        ))}
+      </ul>
+      <button onClick={onResetClick}>Reset</button>
+    </>
+  );
+}
+
+export default function Todo() {
+  const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState(fetchTodos());
+
+  function addTodo() {
+    if (!todoText) {
+      return;
+    }
+    const newTodos = [...todos, { str: todoText, id: nanoid() }];
+    setTodos(newTodos);
+    setTodoText("");
+  }
+
+  function removeTodos(index) {
     const newTodos = todos.slice();
     newTodos.splice(index, 1);
     setTodos(newTodos);
   }
 
-  function setTodosReset() {
+  function resetTodos() {
     const ft = fetchTodos();
     setTodos(ft);
   }
 
-  const handleResetClick = setTodosReset;
-
-  const barContent = useTodoBar(setTodosAdd);
-
-  // render inputs:
-  // - barContent
-  // - handleResetClick
-  // - todos
-  // - setTodosRemove
   return (
-    <>
-      {barContent}
-      <DisplayCheckBoxes {...{ todos, setTodosRemove }} />
-      <DisplayTodos {...{ todos }} />
-      <button onClick={handleResetClick}>Reset</button>
-    </>
+    <TodoPure
+      todoText={todoText}
+      todos={todos}
+      onTodoTextChange={setTodoText}
+      onTodoTextSubmit={addTodo}
+      onIsDoneChange={removeTodos}
+      onResetClick={resetTodos}
+    />
   );
 }
-
-function Todo() {
-  return <TodoLcl />;
-}
-
-export default Todo;
